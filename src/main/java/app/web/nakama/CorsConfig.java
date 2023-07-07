f02b30895cfa95219688fa4f03b444b0bac9f7a7
Kbza <g.leonel.m17@gmail.com>
2023-07-07 05:21:58 -0300

    0.8

--- src/main/java/app/web/nakama/CorsConfig.java
+++ src/main/java/app/web/nakama/CorsConfig.java
@@ -1,17 +1,17 @@
-// package app.web.nakama;
+package app.web.nakama;
 
-// import org.springframework.context.annotation.Configuration;
-// import org.springframework.web.servlet.config.annotation.CorsRegistry;
-// import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-// @Configuration
-// public class CorsConfig implements WebMvcConfigurer {
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
 
-//     @Override
-//     public void addCorsMappings(CorsRegistry registry) {
-//         registry.addMapping("/**")
-//                 .allowedOrigins("https://nakama-0.web.app")
-//                 .allowedMethods("GET", "POST", "PUT", "DELETE")
-//                 .allowedHeaders("*");
-//     }
-// }
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("https://nakama-0.web.app")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*");
+    }
+}
